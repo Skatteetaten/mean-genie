1c8eabb1fe173e030460d094c4bf2d6f8b3971c9
Kim Lindtner <Kim.Lindtner@skatteetaten.no>
2019-09-30 12:25:37 +0200

    wip find application deployments

--- src/main/kotlin/no/skatteetaten/aurora/mean/genie/controller/security/WebSecurityConfig.kt
+++ src/main/kotlin/no/skatteetaten/aurora/mean/genie/controller/security/WebSecurityConfig.kt
@@ -1,11 +1,14 @@
 package no.skatteetaten.aurora.mean.genie.controller.security
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint
 import org.springframework.security.web.util.matcher.RequestMatcher
 import javax.servlet.http.HttpServletRequest
@@ -14,9 +17,18 @@ import javax.servlet.http.HttpServletRequest
 @EnableWebSecurity
 class WebSecurityConfig(
     @Value("\${management.server.port}") val managementPort: Int,
+    @Value("\${mean-genie.username}") val userName: String,
+    @Value("\${mean-genie.password}") val password: String,
+    val passwordEncoder: PasswordEncoder,
     val authEntryPoint: BasicAuthenticationEntryPoint
 
 ) : WebSecurityConfigurerAdapter() {
+
+    @Autowired
+    @Throws(Exception::class)
+    fun configureGlobalSecurity(auth: AuthenticationManagerBuilder) {
+        auth.inMemoryAuthentication().withUser(userName).password(passwordEncoder.encode(password)).roles("USER")
+    }
 
     private fun forPort(port: Int) = RequestMatcher { request: HttpServletRequest -> port == request.localPort }
 
@@ -29,7 +41,7 @@ class WebSecurityConfig(
             .requestMatchers(forPort(managementPort)).permitAll()
             .antMatchers("/docs/index.html").permitAll()
             .antMatchers("/").permitAll()
-            .antMatchers("/api/**").permitAll()
+            .antMatchers("/api/**").hasRole("USER")
             .and().httpBasic().realmName("MEAN-GENIE").authenticationEntryPoint(authEntryPoint)
     }
 }
