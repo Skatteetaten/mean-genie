5e3bd22b1198bde1e29f7c9605d7204e187adf61
Kristoffer Moberg Christensen <kristoffermoberg.christensen@skatteetaten.no>
2019-10-18 15:27:29 +0200

    added custom json deserializer for ApplicationDeployment

--- src/main/kotlin/no/skatteetaten/aurora/mean/genie/crd/ApplicationDeployment.kt
+++ src/main/kotlin/no/skatteetaten/aurora/mean/genie/crd/ApplicationDeployment.kt
@@ -4,10 +4,11 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize
 import io.fabric8.kubernetes.client.CustomResource
+import no.skatteetaten.aurora.mean.genie.service.ApplicationDeploymentDeserializer
 
-@JsonDeserialize
+@JsonDeserialize(using=ApplicationDeploymentDeserializer::class)
 @JsonIgnoreProperties(ignoreUnknown = true)
-data class ApplicationDeployment(val spec: ApplicationDeploymentSpec?) : CustomResource() {
+data class ApplicationDeployment(val spec: ApplicationDeploymentSpec? ) : CustomResource("ApplicationDeployment") {
 
     // var spec: ApplicationDeploymentSpec? = null
 
@@ -17,3 +18,5 @@ data class ApplicationDeployment(val spec: ApplicationDeploymentSpec?) : CustomR
             '}'
     }
 }
+
+
--- src/main/kotlin/no/skatteetaten/aurora/mean/genie/service/ApplicationDeploymentOperator.kt
+++ src/main/kotlin/no/skatteetaten/aurora/mean/genie/service/ApplicationDeploymentOperator.kt
@@ -1,24 +1,35 @@
 package no.skatteetaten.aurora.mean.genie.service
 
-import com.fasterxml.jackson.databind.JsonNode
-import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.core.JsonProcessingException
+import com.fasterxml.jackson.databind.*
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer
+import com.fasterxml.jackson.databind.module.SimpleModule
 import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import io.fabric8.kubernetes.api.model.events.Event
 import io.fabric8.kubernetes.client.CustomResource
 import io.fabric8.kubernetes.client.KubernetesClientException
 import io.fabric8.kubernetes.client.Watcher
 import io.fabric8.kubernetes.client.dsl.NonNamespaceOperation
 import io.fabric8.kubernetes.client.dsl.Resource
+import io.fabric8.openshift.api.model.DeploymentConfig
 import mu.KotlinLogging
 import no.skatteetaten.aurora.mean.genie.crd.ApplicationDeployment
 import no.skatteetaten.aurora.mean.genie.crd.ApplicationDeploymentDoneable
 import no.skatteetaten.aurora.mean.genie.crd.ApplicationDeploymentList
 import org.springframework.stereotype.Service
+import java.io.IOException
+import java.math.BigInteger
+
 
 private val logger = KotlinLogging.logger {}
 
 @Service
 class ApplicationDeploymentOperator(
-    private val appCRDClient: NonNamespaceOperation<ApplicationDeployment, ApplicationDeploymentList, ApplicationDeploymentDoneable, Resource<ApplicationDeployment, ApplicationDeploymentDoneable>>? = null
+        private val appCRDClient: NonNamespaceOperation<ApplicationDeployment, ApplicationDeploymentList, ApplicationDeploymentDoneable, Resource<ApplicationDeployment, ApplicationDeploymentDoneable>>? = null
 ) {
 
     fun init(): Boolean {
@@ -29,17 +40,40 @@ class ApplicationDeploymentOperator(
     }
 }
 
+
 class ApplicationDeploymentWatcher : Watcher<ApplicationDeployment> {
     override fun eventReceived(action: Watcher.Action?, resource: ApplicationDeployment?) {
-        if (action == Watcher.Action.DELETED) {
-            logger.info(">> Deleting App: " + resource?.metadata?.name)
-            // logger.info(application.toString())
-            // logger.info(application.metadata.labels.getOrDefault("affiliation","Did not find affiliation"))
-            // logger.info(application.spec)
-        }
+        logger.info { resource }
     }
 
     override fun onClose(cause: KubernetesClientException?) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
+}
+
+
+class ApplicationDeploymentDeserializer : JsonDeserializer<ApplicationDeployment>() {
+
+    @Throws(IOException::class, JsonProcessingException::class)
+    override fun deserialize(jsonParser: JsonParser, deserializationContext: DeserializationContext): ApplicationDeployment {
+        return objectMapper.readValue(jsonParser, ApplicationDeployment::class.java)
+    }
+
+    @JsonDeserialize
+    private interface DefaultJsonDeserializer// Reset default json deserializer
+
+    companion object {
+
+        private val objectMapper = ObjectMapper()
+
+        init {
+            val module = SimpleModule()
+            objectMapper.apply {
+                this.registerModule(module)
+                        .addMixIn(ApplicationDeployment::class.java, DefaultJsonDeserializer::class.java).registerKotlinModule()
+                this.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+            }
+        }
+    }
+
 }
