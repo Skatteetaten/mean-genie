46c9d31e1ade191a0108716d50222d24ef38787b
Kim Lindtner <Kim.Lindtner@skatteetaten.no>
2019-10-24 10:12:19 +0200

    working with tests

--- src/test/kotlin/no/skatteetaten/aurora/mean/genie/service/ApplicationDeploymentWatcherTest.kt
+++ src/test/kotlin/no/skatteetaten/aurora/mean/genie/service/ApplicationDeploymentWatcherTest.kt
@@ -1,34 +1,31 @@
 package no.skatteetaten.aurora.mean.genie.service
-//
-// import assertk.assertThat
-// import assertk.assertions.isEqualTo
-// import assertk.assertions.isInstanceOf
-// import com.fasterxml.jackson.module.kotlin.readValue
-// import io.fabric8.kubernetes.api.model.WatchEvent
-// import io.fabric8.kubernetes.client.utils.Serialization
-// import no.skatteetaten.aurora.mean.genie.ApplicationConfig
-// import no.skatteetaten.aurora.mean.genie.model.ApplicationDeployment
-// import no.skatteetaten.aurora.mean.genie.model.ApplicationDeploymentSpec
-// import org.junit.jupiter.api.Test
-//
-// class ApplicationDeploymentWatcherTest {
-//     @Test
-//     fun `deserialize watch event`() {
-//         ApplicationConfig()
-//         val applicationDeploymentSpec = ApplicationDeploymentSpec().apply {
-//             applicationDeploymentId = "123"
-//             applicationDeploymentName = "abc"
-//             databases = listOf("DB1")
-//         }
-//         val applicationDeployment = ApplicationDeployment().apply {
-//             spec = applicationDeploymentSpec
-//         }
-//
-//         val watchEvent = WatchEvent(applicationDeployment, "ADDED")
-//         val json = Serialization.asJson(watchEvent)
-//         val readValue = Serialization.jsonMapper().readValue<WatchEvent>(json)
-//
-//         assertThat(readValue.type).isEqualTo("ADDED")
-//         assertThat(readValue.`object`).isInstanceOf(ApplicationDeployment::class)
-//     }
-// }
+
+import assertk.assertThat
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import io.fabric8.kubernetes.api.model.ObjectMeta
+import no.skatteetaten.aurora.mean.genie.ApplicationConfig
+import no.skatteetaten.aurora.mean.genie.model.ApplicationDeployment
+import no.skatteetaten.aurora.mean.genie.model.ApplicationDeploymentSpec
+import no.skatteetaten.aurora.mean.genie.model.WatchEvent
+import org.junit.jupiter.api.Test
+
+class ApplicationDeploymentWatcherTest {
+    @Test
+    fun `deserialize watch event`() {
+        ApplicationConfig()
+
+        val watchEvent = WatchEvent<ApplicationDeployment>(
+            "DELETED",
+            ApplicationDeployment(ApplicationDeploymentSpec("123", "abc"), ObjectMeta())
+        )
+
+        val json = ObjectMapper().writeValueAsString(watchEvent)
+        println(json)
+        val readValue = ObjectMapper().readValue<WatchEvent>(json)
+        println(readValue)
+
+        assertThat(readValue.type).isEqualTo("ADDED")
+        assertThat(readValue.`object`).isInstanceOf(ApplicationDeployment::class)
+    }
+}
